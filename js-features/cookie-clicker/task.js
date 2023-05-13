aabb9745f5020bcf657fffb2833d916df2283f3d
Danvintius <danila.94@mail.ru>
2023-05-14 00:47:15 +0300

    Вторая задача готова

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,6 +1,15 @@
-const clicker = document.querySelector('.clicker__cookie')
+const clicker = document.getElementById('cookie')
+const count = document.getElementById('clicker__counter')
 clicker.onclick = function() {
-  let count = document.getElementById('clicker__counter')
-  count.textContent++;
+  let clicks = count.textContent++;
+  if (clicks % 2 == 0) {
+  clicker.width = 300;
+  clicker.height = 214;
+  } else {
+  clicker.width = 200;
+  clicker.height = 127;
   }
+  }
+
+
 
