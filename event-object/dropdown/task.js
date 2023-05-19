b53bed3ab5fe9b2bb40e85ae7cbab2a5846caaa7
Danvintius <danila.94@mail.ru>
2023-05-19 16:14:03 +0300

    dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,14 @@
+const dValue = document.querySelector('.dropdown__value')
+const list = document.querySelector('.dropdown__list')
+let item = document.querySelectorAll('.dropdown__item')
+dValue.onclick = function() {
+  list.classList.toggle('dropdown__list_active');
+}
+
+for (let i = 0; i < item.length; i++) {
+  item[i].onclick = function() {
+    list.classList.remove('dropdown__list_active');
+    dValue.textContent = item[i].textContent;
+}
+  continue;
+}
