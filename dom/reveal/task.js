d3fa4568aeb06610b02da7aa7f7002e815c8da18
Danvintius <danila.94@mail.ru>
2023-05-26 19:00:59 +0300

    reveal2

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,5 +1,19 @@
-const reveal = document.querySelector('.reveal');
+const revealE = document.querySelectorAll('.reveal');
+
+const isInViewport = function(element){
+   const viewportHeight = window.innerHeight;
+   const elementTop = element.getBoundingClientRect().top;
+   const elementBottom = element.getBoundingClientRect().bottom;
+
+   return viewportHeight > elementTop && elementBottom > 0;
+};
 
 document.onscroll = function() {
-   reveal.classList.add('reveal_active');
+   revealE.forEach(element => {
+       if(isInViewport(element)) {
+           element.classList.add('reveal_active');
+       } else {
+           element.classList.remove('reveal_active');
+       }
+   });
 }
