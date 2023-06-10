dd3a7d2fe873d602de2dc95918b3b85bf56afe17
Danvintius <danila.94@mail.ru>
2023-06-10 20:29:06 +0300

    text-editor

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,10 @@
+const textarea = document.getElementById('editor');
+
+textarea.oninput = function() {
+    let value = textarea.value;
+    localStorage.setItem('value', value)
+}
+
+
+const storedText = localStorage.getItem('value');
+textarea.value = storedText;
