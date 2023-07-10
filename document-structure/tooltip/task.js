613260ef0899b1eb0a415ef693370627fe7c9d96
Danvintius <danila.94@mail.ru>
2023-07-11 00:16:16 +0300

    tooltips

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,8 +1,28 @@
-const hasTooltip = document.querySelectorAll('.has-tooltip')
+let tooltips = document.querySelectorAll(".has-tooltip");
 
-for (let i = 0; i < hasTooltip.length; i++) {
-    hasTooltip[i].onclick = function() {
-        hasTooltip[i].classList.add('tooltip_active');
-        return false;
-    }
-}
+tooltips.forEach(tooltip => {
+   const elem = document.createElement('div');
+   elem.textContent = tooltip.title;
+
+   tooltip.addEventListener("click", (e) => {
+      e.preventDefault();
+      elem.classList.add("tooltip");
+      tooltip.insertAdjacentElement('beforeBegin', elem);
+      elem.style.position = "absolute";
+      //elem.style.left = `${tooltip.getBoundingClientRect().left}px`;
+      //elem.style.top = `${tooltip.getBoundingClientRect().bottom}px`;
+
+      const elems = Array.from(document.querySelectorAll(".tooltip_active"));
+      const findElem = elems.find(elem => elem.classList.contains("tooltip_active"));
+
+      if (findElem === undefined) {
+         elem.classList.add("tooltip_active");
+      } else {
+         if (findElem !== elem) {
+            elem.classList.add("tooltip_active");
+         }
+         findElem.classList.remove("tooltip_active");
+      }
+   }
+   )
+})
