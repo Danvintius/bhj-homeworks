5ac64343926c2f8422f8780aa2efe4a0bbb388c7
Danvintius <danila.94@mail.ru>
2023-07-30 21:58:52 +0300

    todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,11 +2,20 @@ const button = document.getElementById('tasks__add');
 const tasksList = document.getElementById('tasks__list');
 const input = document.getElementById('task__input');
 
-button.onclick = function() {
-    if (input.value != '') {
-        let element = document.createElement('div');
-        tasksList.insertAdjacentHTML("beforeEnd", "<div class="task"><div class="task__title"></div><a href="#" class="task__remove">&times;</a></div>");
-        const taskTitle = document.querySelectorAll('.task__title');
-        taskTitle.textContent = input.value;
-    }
-}
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    let inputValue = input.value;
+    if (inputValue.trim() != '') {
+        tasksList.insertAdjacentHTML("beforeEnd", `<div class="task">
+        <div class="task__title">
+         ${inputValue}
+       </div >
+           <a href="#" class="task__remove">&times;</a>
+     </div >`)
+        const task = Array.from(document.querySelectorAll(".task__remove"));
+        task[task.length - 1].addEventListener("click", () => {
+           tasksList.removeChild(task[task.length - 1].closest(".task"));
+        })
+        inputValue = '';
+     }
+})
